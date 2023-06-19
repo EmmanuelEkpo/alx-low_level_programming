cd92e8de720d9f1e6095b95de064bae26a93f5f1
Emmanuel Ekpo <nuelekpo@gmail.com>
2023-06-19 10:37:34 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Invariably 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	char n;
+
+	char o;
+
+	m = 'a' <= 'z';
+	n = 'q';
+	o = 'e';
+	while
+		(m != n && m != o) {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
