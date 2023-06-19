b9ccecaa2518b803e28f19cd94a634945bb70fba
Emmanuel Ekpo <nuelekpo@gmail.com>
2023-06-19 12:34:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Invariably 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	int y = 10;
+
+	if (x < y)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
