058c9a49771d1b297776ef91542e0b768eb54c46
Emmanuel Ekpo <nuelekpo@gmail.com>
2023-06-20 14:36:38 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isalpha - Function that checks for alphabetic character
+ * @c: Character in ASCII code
+ * Return: 1 if it is an alphabet, 0 for others
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
