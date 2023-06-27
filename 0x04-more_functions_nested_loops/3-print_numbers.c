1eb90f6ea1baf69bf9b607e2096a978721213e1f
Emmanuel Ekpo <nuelekpo@gmail.com>
2023-06-27 12:42:31 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function that prints numbers
+ * Return: Invariably 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int m;
+
+	for (m = 48; m <= 57; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
