bece98132b191e058a08d2344b162558f5065fe4
Emmanuel Ekpo <nuelekpo@gmail.com>
2023-06-29 12:54:25 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,17 @@ char *_strcat(char *dest, char *src)
 	int x;
 	int y;
 
-	i = 0;
+	x = 0;
 	while (dest[x] != '\0')
 	{
 		x++;
 	}
-	j = 0;
+	y = 0;
 	while (src[y] != '\0')
 	{
 		dest[x] = src[y];
-		i++;
-		j++;
+		x++;
+		y++;
 	}
 
 	dest[x] = '\0';
