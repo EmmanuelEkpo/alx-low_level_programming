06c2a3f4e39262535dd9ff7e6a7314d5238f0826
Emmanuel Ekpo <nuelekpo@gmail.com>
2023-07-20 10:45:17 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1;
+	int digit2;
+	int comma;
+
+	comma = 44;
+	for (digit1 = 48; digit1 <= 57; digit1++)
+	{
+		for (digit2 = 49; digit2 <= 57; digit2++)
+		{
+			if (digit1 != digit2 && digit2 > digit1)
+			{
+				putchar(digit1);
+				putchar(digit2);
+				putchar(comma);
+				putchar(' ');
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
